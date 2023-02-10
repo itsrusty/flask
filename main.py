b1ae20d24f44405d416244ad5d2a2543f706f452
itsrustycode <82051017+itsrusty@users.noreply.github.com>
2023-02-09 17:54:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from flask import Flask, jsonify
 import os
+# from config import config
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"message": "testing_message"})
 
+@app.route("/testing")
+def testing():
+    return "testing web app"
+    
+@app.route("/add/<string:product_name>", methods=['POST'])
+def add(product_name):
+    # pass
+    if product_name == "hiram":
+        return jsonify({"message": "testeando app"})
+    else:
+        return jsonify({"message": "test"})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+if __name__ == "__main__":
+    # app.run(debug=True)
+     app.run(debug=True, port=os.getenv("PORT", default=5000))
